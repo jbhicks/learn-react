2f487c5bab995716e765fa9b9de6bf151cd034c4
Josh Hicks <joshua.hicks@schwab.com>
2020-08-16 11:42:07 -0700

    starting on ch. 3

---
+++ jsx/src/index.js
@@ -0,0 +1,27 @@
+// Import the React and React DOM Libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a React component
+const App = function () {
+    const buttonText = { text: 'Click me'};
+    const style = { backgroundColor: 'blue', color: 'white'};
+    const labelText = "Enter name: "
+
+    return (
+        <div>
+            <label className="label" htmlFor="name">
+                {labelText}
+            </label>
+            <input id="name" type="text"/>
+            <button style={style}>
+                {buttonText.text}
+            </button>
+        </div>
+    )};
+
+// Take the React component and show it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
